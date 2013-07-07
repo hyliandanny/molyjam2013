b0b2edf9faa20569e43adb98c008f6b41337dec2
Devin Rosen <devinprosen@gmail.com>
2013-07-07 12:03:34 -0700

    hils.

--- Unity/Assets/Scripts/Scenery.cs
+++ Unity/Assets/Scripts/Scenery.cs
@@ -4,13 +4,9 @@ using System.Collections;
 public class Scenery : MonoBehaviour {
 	
 	void Awake() {
-		//Messenger.AddListener(typeof(PickupCollectedMessage),HandlePickupCollectedMessage);
-		//Messenger.AddListener(typeof(NextPlatformReachedMessage),HandleNextPlatformReachedMessage);
 		Messenger.AddListener(typeof(ColorMessage),HandleColorMessage);
 	}
 	void OnDestroy() {
-		//Messenger.RemoveListener(typeof(PickupCollectedMessage),HandlePickupCollectedMessage);
-		//Messenger.RemoveListener(typeof(NextPlatformReachedMessage),HandleNextPlatformReachedMessage);
 		Messenger.RemoveListener(typeof(ColorMessage),HandleColorMessage);
 	}
 	void HandleColorMessage(Message msg) {
@@ -18,45 +14,5 @@ public class Scenery : MonoBehaviour {
 		if(message != null) {
 			GetComponent<ColorMix>().SetColor(message.R,message.G,message.B);
 		}
-	}
-	void HandlePickupCollectedMessage(Message msg) {
-		PickupCollectedMessage message = msg as PickupCollectedMessage;
-		if(message != null) {
-			ColorMix cm = GetComponent<ColorMix>();
-			if(nextPlatformReached) {
-				nextPlatformReached = false;
-				cm.rValue = (message.Pickup.pickupType == PickupType.R);
-				cm.gValue = (message.Pickup.pickupType == PickupType.G);
-				cm.bValue = (message.Pickup.pickupType == PickupType.B);
-			}
-			else {
-				if(message.Pickup.pickupType == PickupType.R) {
-					cm.rValue = true;
-				}
-				else if(message.Pickup.pickupType == PickupType.G) {
-					cm.gValue = true;
-				}
-				else if(message.Pickup.pickupType == PickupType.B) {
-					cm.bValue = true;
-				}
-			}
-			
-		}
-	}
-	bool nextPlatformReached = true;
-	void HandleNextPlatformReachedMessage(Message msg) {
-		NextPlatformReachedMessage message = msg as NextPlatformReachedMessage;
-		if(message != null) {
-			nextPlatformReached = true;
-		}
-	}
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
 	}
 }
