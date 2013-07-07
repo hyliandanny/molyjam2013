aa56b7c92a7f30c7df9aee5a3c336be520118a9f
Charles Parks <c0rphan@gmail.com>
2013-07-07 13:19:06 -0700

    Fixing some game over camera stuff, hopefully.

--- Unity/Assets/Scripts/GameOver.cs
+++ Unity/Assets/Scripts/GameOver.cs
@@ -11,16 +11,11 @@ public class GameOver : MonoBehaviour {
 	void OnEnable () {
 		Vector3 cameraPos = Camera.main.transform.position;
 		cameraPos.x = LevelSectionTracker.instance.spawnPoint.position.x;
-		cameraPos.y = LevelSectionTracker.instance.spawnPoint.position.y;
+		cameraPos.y = LevelSectionTracker.instance.spawnPoint.position.y + 1f;
 		Camera.main.transform.position = cameraPos;
 		PlayerFollow camFollow = Camera.main.GetComponent<PlayerFollow>();
 		camFollow.enabled = false;
 		//miniMapCamera.gameObject.SetActive(false);
-		if( CharacterController2D.screen != null) {
-			byte[] bytes = CharacterController2D.screen.EncodeToPNG();
-			System.IO.File.WriteAllBytes(Application.dataPath+"/levelScreenshot-"+System.DateTime.Now.ToString("yyyyMMddhhmmss")+".png", bytes);
-			DestroyObject(CharacterController2D.screen);
-		}
 		StartCoroutine(PanCamera());
 	}
 		
