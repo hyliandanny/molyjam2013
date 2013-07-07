66a8b6430a3944600207f77a3ac4f0702007d653
amsmntparks <danny.parks@gmail.com>
2013-07-07 11:11:17 -0700

    fixed border image alignment
    also tweaked the random scaling a bit

--- Unity/Assets/Scripts/FillScreen.cs
+++ Unity/Assets/Scripts/FillScreen.cs
@@ -2,7 +2,9 @@ using UnityEngine;
 using System.Collections;
 
 public class FillScreen : MonoBehaviour {
-
+	
+	public bool gamePlayScene = false;
+	
 	// Use this for initialization
 	void Start () {
 	
@@ -19,6 +21,12 @@ public class FillScreen : MonoBehaviour {
         float h = Mathf.Tan(cam.fov*Mathf.Deg2Rad*0.5f)*pos*112f;
  
         transform.localScale = new Vector3(h*cam.aspect,h,0f);
+		
+		if(gamePlayScene){
+			Vector3 nScale = transform.localScale;
+			nScale.x = nScale.x/5f;
+			transform.localScale = nScale;
+		}
 		
 	}
 }
