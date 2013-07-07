a4ac496275a8f7d7a6ec053890e7545b843824d5
Charles Parks <c0rphan@Charless-MacBook-Pro.local>
2013-07-06 23:57:07 -0700

    Don't die!

---
+++ Unity/Assets/Scripts/DeathBoxFollow.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using System.Collections;
+
+public class PlayerFollow : MonoBehaviour {
+	
+	public Transform character;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 myPos = transform.position;
+		myPos.x = character.position.x;
+		//myPos.y = character.position.y;
+		transform.position = myPos;
+	}
+}
