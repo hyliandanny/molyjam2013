2afd0ded4069b9ede57ab0e29eeedd0f689ff177
Charles Parks <c0rphan@gmail.com>
2013-07-07 11:52:30 -0700

    Color status bar and blissing out!

---
+++ Unity/Assets/Scripts/MessageSystem/Messages/BlissedOutMessage.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+using System.Collections;
+
+public class BlissedOutMessage : Message {
+
+	public bool blissingOut;
+	
+	public BlissedOutMessage(bool toggle) {
+		blissingOut = toggle;
+	}
+	
+}
