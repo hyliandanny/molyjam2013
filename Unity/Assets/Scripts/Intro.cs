f060f81994643f99360f678ce5e85939a584d023
Charles Parks <c0rphan@gmail.com>
2013-07-10 00:16:52 -0700

    Modified Intro script to be much less abrupt.

--- Unity/Assets/Scripts/Intro.cs
+++ Unity/Assets/Scripts/Intro.cs
@@ -8,20 +8,30 @@ public class Intro : MonoBehaviour {
 
 	// Play the intro bit
 	public IEnumerator PlayIntro () {
+		yield return new WaitForSeconds(0);
 		camFollow.enabled = false;
 		Vector3 playerPos = transform.position;
-		playerPos.z = Camera.main.transform.position.z;
+		Vector3 camPos = Camera.main.transform.position;
+		playerPos.z = camPos.z;
 		Camera.main.transform.position = playerPos;
 		float orthoSize = 0.1f;
 		Camera.main.orthographicSize = orthoSize;
+		int frameCount = 0;
 		while(orthoSize < 10f) {
 			yield return new WaitForSeconds(0);
+			frameCount++;
+			playerPos = transform.position;
+			playerPos.z = camPos.z;
 			orthoSize += 0.1f;
+			playerPos.x += 0.07f * frameCount;
+			playerPos.y += 0.015f * frameCount;
 			Camera.main.orthographicSize = orthoSize;
+			Camera.main.transform.position = playerPos;
+			thePlayer.controller.SimpleMove(new Vector3(0,-0.07f,0));
 		}
 		camFollow.enabled = true;
 		thePlayer.enabled = true;
-		thePlayer.controller.enabled = true;
+		//thePlayer.controller.enabled = true;
 	}
 	
 	// Update is called once per frame
